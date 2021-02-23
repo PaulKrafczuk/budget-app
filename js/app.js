873b6a0ce172173a4533a54b27794d2f0ae45fa5
PaulKrafczuk <krafczukpaul@gmail.com>
2021-02-23 19:11:19 -0300

    se muestra el titulo del gasto y su valor

--- js/app.js
+++ js/app.js
@@ -15,6 +15,7 @@ class UI {
     this.itemList = [];
     this.itemID = 0;
   }
+
   // submit budget method
   submitBudgetForm() {
     console.log('entra a la funcion submitBudgetForm');
@@ -45,13 +46,68 @@ class UI {
     if (total < 0) {
       this.balance.classList.remove('showGreen', 'showBlack');
       this.balance.classList.add('showRed');
-    }else if (total > 0) {
+    } else if (total > 0) {
       this.balance.classList.remove('showRed', 'showBlack');
       this.balance.classList.add('showGreen');
-    }else if (total === 0) {
+    } else if (total === 0) {
       this.balance.classList.remove('showRed', 'showGreen');
       this.balance.classList.add('showBlack');
     }
+  }
+
+  // submit expense form
+  submitExpenseForm() {
+    const expenseValue = this.expenseInput.value; //capturo el texto del expense
+    const amountValue = this.amountInput.value; //capturo el numero del expense
+    if (expenseValue === '' || amountValue === '' || amountValue < 0) {
+
+      this.expenseFeedback.classList.add('showItem');
+      this.expenseFeedback.innerHTML = `<p>Expense can not be empty or negative</p>`;
+
+      const self = this; //arrastro el this hasta la funcion setTimeOut
+
+      setTimeout(function () {
+        self.expenseFeedback.classList.remove('showItem');
+        self.amountInput.value = '';
+      }, 3000)
+    } else {
+      let amount = parseInt(amountValue); // amount value era string y lo hago number
+      this.expenseInput.value = "";
+      this.amountInput.value = "";
+
+      let expense = {
+        id: this.itemID,
+        title: expenseValue,
+        amount: amount
+      }
+
+      this.itemID++;
+      this.itemList.push(expense);
+      this.addExpense(expense);
+    }
+  }
+
+  //add expense
+  addExpense(expense) {
+    const div = document.createElement('div');
+    div.classList.add('expense');
+    div.innerHTML = `<div class="expense-item d-flex justify-content-between align-items-baseline">
+
+  <h6 class="expense-title mb-0 text-uppercase list-item">${expense.title}</h6>
+  <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+
+  <div class="expense-icons list-item">
+
+   <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+    <i class="fas fa-edit"></i>
+   </a>
+   <a href="#" class="delete-icon" data-id="${expense.id}">
+    <i class="fas fa-trash"></i>
+   </a>
+  </div>`;
+
+  this.expenseList.appendChild(div);
+
   }
 
   // total expense
@@ -78,6 +134,7 @@ function eventListeners() {
   //expense form submit
   expenseForm.addEventListener('submit', function (event) {
     event.preventDefault();
+    ui.submitExpenseForm();
   })
 
   //expense click
